e1fdbbf14d25087cfc3ed2d1d98e1745b0b367e1
Vishakha Devi <78592565+Vishakha2072@users.noreply.github.com>
2022-07-10 16:14:30 +0530

    Create pattern4.cpp

---
+++ pattern4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=n;
+    while(i>=1){
+        int j=n;
+        while(j>=1){
+            cout<<j<<" ";
+            j--;
+        }
+        cout<<endl;
+        i--;
+    }
+    return 0;
+}
